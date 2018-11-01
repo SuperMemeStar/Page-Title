32f3b5d9e491afda39d4699a5cee2e8a889e17d0
Khaliyl <32131400+SuperMemeStar@users.noreply.github.com>
2018-11-01 09:58:11 -0400

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,11 @@
+$("#ice").click(function() {
+    $("#cold").fadeToggle();
+});
+
+$("#save").click(function() {
+    $("#hero").slideToggle();
+});
+
+$("#build").click(function() {
+    $("#house").fadeToggle();
+});
